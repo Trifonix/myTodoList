75b4a9331d8db3d7e908543a22a9956e1bd6a66f
Trifonix <trifonix@rocketmail.com>
2024-04-30 23:12:22 +0300

    upd delete from db function

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,13 @@
 <script setup>
 /* import ref, uuid, firebase */
 import { ref, onMounted } from "vue";
-import { collection, onSnapshot, addDoc } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import { db } from "@/firebase";
 /* firebase ref */
 const todosCollectionRef = collection(db, "todos");
@@ -97,12 +103,11 @@ const addTodo = () => {
     content: newTodoContent.value,
     done: false,
   });
-  todos.value.unshift(newTodo);
   newTodoContent.value = "";
 };
 /* todo delete */
 const deleteTodo = (id) => {
-  todos.value = todos.value.filter((todo) => todo.id !== id);
+  deleteDoc(doc(todosCollectionRef, id));
 };
 /* toggle done */
 const toggleDone = (id) => {
