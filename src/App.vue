fc2a4791e8d0192a0aef7a5cecc08f96379a800b
Trifonix <trifonix@rocketmail.com>
2024-04-30 16:47:33 +0300

    add new task form

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,22 @@
-<template>Hello WORLD!</template>
+<template>
+  <div class="wrapper-todo">
+    <div class="title has-text-centered">My ToDo List!</div>
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="New task" />
+      </p>
+      <p class="control">
+        <button class="button is-info">Add</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "bulma/css/bulma.min.css";
+.wrapper-todo {
+  min-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
