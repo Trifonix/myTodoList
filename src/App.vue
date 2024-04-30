1ca733745373e1aea09857a17cb4a46c3afe459e
Trifonix <trifonix@rocketmail.com>
2024-04-30 17:04:21 +0300

    add card and style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="wrapper-todo">
     <div class="title has-text-centered">My ToDo List!</div>
-    <div class="field is-grouped">
+    <div class="field is-grouped mb-5">
       <p class="control is-expanded">
         <input class="input" type="text" placeholder="New task" />
       </p>
       <p class="control">
         <button class="button is-info">Add</button>
       </p>
+    </div>
+    <div v-for="i in 3" :key="i" class="card mb-3">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">Try to finish work!</div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">&check;</button>
+              <button class="button is-danger ml-3">&cross;</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
